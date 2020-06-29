40dd5375c5589314744dfb00b601579feb2e75d3
Nelson508 <nelson.dominguez1601@alumnos.ubiobio.cl>
2020-06-29 01:52:37 -0400

    datos DB

--- global/config.php
+++ global/config.php
@@ -1,11 +1,16 @@
 <?php
-define("KEY","develoteca");
+/*define("KEY","develoteca");
 define("COD","AES-128-ECB");
 
 define("SERVIDOR","localhost");
 define("USUARIO","root");
 define("PASSWORD","");
-define("DB","gift.com");
+define("DB","gift.com");*/
+
+define("SERVIDOR","mysql.face.ubiobio.cl");
+define("USUARIO","grup7co");
+define("PASSWORD","bdgrup7co");
+define("DB","softbd7co");
 
 
 
